945c8a54afd922025a88a7821ade68166ba4e197
Ariel Serato <Serato.arieli@gmail.com>
2021-12-24 11:48:26 -0300

    a ver si funciona

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 
-app.use(express)
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', Router)
+
+app.listen(4000, () => {
+    console.log('server is listening on port 4000')
+})

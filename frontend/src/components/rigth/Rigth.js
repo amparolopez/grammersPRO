91e1b5230ba17802a671e58efcfac715ca479b4d
FedericoLQ <fedelima19@gmail.com>
2021-12-24 16:22:43 -0300

    Step 1

--- frontend/src/components/rigth/Rigth.js
+++ frontend/src/components/rigth/Rigth.js
@@ -1,7 +1,15 @@
 import "./Rigth.css"
+import { FaRegBell, FaCloudUploadAlt } from "react-icons/fa";
 const Rigth = () => {
     return (
         <div className="rigthUsers">
+            <div className="Searchs">
+            <input placeholder=" 🔍 Search" className="inputSearch" type="text"></input>
+           <FaRegBell className="bell"/>
+            <FaCloudUploadAlt className="bell"/>
+
+            </div>
+            
             
         </div>
     )

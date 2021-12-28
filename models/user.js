1af5de984ab7c54021bc28410eb0b39df9be169f
LeoFormin1 <forminileonel@gmail.com>
2021-12-28 12:15:19 -0300

    x

--- models/user.js
+++ models/user.js
@@ -2,16 +2,5 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-    userName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    imgUrl: { type: String, required: true },
-    country: { type: String, required: true },
-    googleFlag: {type: Boolean, default: false},
-    followUsers:[{ type:mongoose.Types.ObjectId, ref:'user'}]
+    
 })
-
-const User = mongoose.model('user', userSchema)
-
-module.exports = User;
